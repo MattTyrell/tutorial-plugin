fa439f5e653fa0af48384a48bc0ee8576a70b999
MattTyrell <matthew@tyrellsys.com.my>
2025-01-09 11:11:50 +0800

    refs #99958 Added bake migrations and controller files.

---
+++ tests/TestCase/Controller/ItemsControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace ItemManager\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+use ItemManager\Controller\ItemsController;
+
+/**
+ * ItemManager\Controller\ItemsController Test Case
+ *
+ * @uses \ItemManager\Controller\ItemsController
+ */
+class ItemsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+}
