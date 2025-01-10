48954aa756b1c149a81c311c5e273b7e50bc872c
MattTyrell <matthew@tyrellsys.com.my>
2025-01-10 11:36:54 +0800

    refs #99958 Added initialize method of AppController.
    Also fixed importing for base class to inherit.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,8 +3,31 @@ declare(strict_types=1);
 
 namespace ItemManager\Controller;
 
-use App\Controller\AppController as BaseController;
+use Cake\Controller\Controller;
 
-class AppController extends BaseController
+class AppController extends Controller
 {
+    /**
+     * Initialization hook method.
+     *
+     * Use this method to add common initialization code like loading components.
+     *
+     * e.g. `$this->loadComponent('FormProtection');`
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('Flash');
+
+        /*
+         * Enable the following component for recommended CakePHP form protection settings.
+         * see https://book.cakephp.org/4/en/controllers/components/form-protection.html
+         */
+        //$this->loadComponent('FormProtection');
+    }
 }
