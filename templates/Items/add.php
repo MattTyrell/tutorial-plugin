26c26b3d3ccd0aa2bb50dd755d35b0239f132373
MattTyrell <matthew@tyrellsys.com.my>
2025-01-10 11:40:37 +0800

    refs #99958 Added bake template files for Items and Categories.

---
+++ templates/Items/add.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \Cake\Datasource\EntityInterface $item
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Items'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="items form content">
+            <?= $this->Form->create($item) ?>
+            <fieldset>
+                <legend><?= __('Add Item') ?></legend>
+                <?php
+                    echo $this->Form->control('item_title');
+                    echo $this->Form->control('sale_price');
+                    echo $this->Form->control('is_in_stock');
+                    echo $this->Form->control('item_description');
+                    echo $this->Form->control('category_id');
+                    echo $this->Form->control('deleted', ['empty' => true]);
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
