b8567bbbf4b858b4511fb039e12a2235e8f3ce7a
MattTyrell <matthew@tyrellsys.com.my>
2025-01-10 16:13:35 +0800

    refs #99958 Added data provider and test case for items validation.

--- tests/TestCase/Model/Table/ItemsTableTest.php
+++ tests/TestCase/Model/Table/ItemsTableTest.php
@@ -55,12 +55,17 @@ class ItemsTableTest extends TestCase
     /**
      * Test validationDefault method
      *
+     * @dataProvider dataProviderValidationDefault
+     * @param array $data The data to be validated
+     * @param array $expected The expected validation errors
      * @return void
      * @uses \ItemManager\Model\Table\ItemsTable::validationDefault()
      */
-    public function testValidationDefault(): void
+    public function testValidationDefault(array $data, array $expected): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $validator = $this->Items->getValidator('default');
+        $result = $validator->validate($data);
+        $this->assertEquals($expected, $result);
     }
 
     /**
@@ -72,5 +77,40 @@ class ItemsTableTest extends TestCase
     public function testBuildRules(): void
     {
         $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Data provider for validationDefault test
+     *
+     * @return array
+     */
+    public function dataProviderValidationDefault(): array
+    {
+        return [
+            [
+                'data' => [
+                    'name' => 'Item 1',
+                    'sale_price' => 35.5,
+                    'is_in_stock' => 1,
+                    'item_description' => '',
+                    'category_id' => 1,
+                ],
+                'expected' => [],
+            ],
+            [
+                'data' => [
+                    'name' => '',
+                    'sale_price' => 'Hello',
+                    'is_in_stock' => 67,
+                    'item_description' => '',
+                    'category_id' => 2,
+                ],
+                'expected' => [
+                    'name' => ['_empty' => 'This field cannot be left empty'],
+                    'sale_price' => ['_empty' => 'This field cannot be left empty'],
+                    'is_in_stock' => ['_boolean' => 'This field must be a boolean value'],
+                ],
+            ]
+        ];
     }
 }
