eb9d29341b04530cd221d7492de2b51cd89ba6dc
MattTyrell <matthew@tyrellsys.com.my>
2025-01-10 10:38:28 +0800

    refs #99958 Added Items and Categories controller

---
+++ tests/TestCase/Controller/CategoriesControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace ItemManager\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+use ItemManager\Controller\CategoriesController;
+
+/**
+ * ItemManager\Controller\CategoriesController Test Case
+ *
+ * @uses \ItemManager\Controller\CategoriesController
+ */
+class CategoriesControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+}
