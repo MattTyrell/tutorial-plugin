770ae02e6fd1d1376d0008ed99965a5df42ed458
MattTyrell <matthew@tyrellsys.com.my>
2025-01-15 16:35:21 +0800

    refs #99958 Fixed and improved the adapter check.

--- config/Migrations/20250109072524_CreateCategories.php
+++ config/Migrations/20250109072524_CreateCategories.php
@@ -16,7 +16,8 @@ class CreateCategories extends AbstractMigration
      */
     public function up(): void
     {
-        $isSqlite = $this->getAdapter()->getOption('name') === 'sqlite';
+        $connection = $this->getAdapter()->getConnection();
+        $isSqlite = $connection->getAttribute(PDO::ATTR_DRIVER_NAME) === 'sqlite';
         $table = $this->table('categories')
             ->addColumn('id', $isSqlite ? 'integer' : 'biginteger', [
                 'autoIncrement' => true,
