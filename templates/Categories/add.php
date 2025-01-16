bfc8aca736fcd9e7b73adbe8191c62dc0733c53a
MattTyrell <matthew@tyrellsys.com.my>
2025-01-16 11:54:17 +0800

    refs #99958 Added missing semicolon in Categories add template.

--- templates/Categories/add.php
+++ templates/Categories/add.php
@@ -13,7 +13,7 @@
     </aside>
     <div class="column-responsive column-80">
         <div class="categories form content">
-            <?= $this->Form->create($category) ?>
+            <?= $this->Form->create($category); ?>
             <fieldset>
                 <legend><?= __('Add Category') ?></legend>
                     <?= $this->Form->control('name'); ?>
