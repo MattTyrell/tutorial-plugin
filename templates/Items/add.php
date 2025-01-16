927a605c2496c8086149eb7f98f2d0bda458700b
MattTyrell <matthew@tyrellsys.com.my>
2025-01-16 12:57:27 +0800

    refs #99958 Cleaned up Items template files.
    Added semicolons.
    Fixed indentation, made long lines into multi-lines.

--- templates/Items/add.php
+++ templates/Items/add.php
@@ -7,23 +7,23 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Items'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <h4 class="heading"><?= __('Actions'); ?></h4>
+            <?= $this->Html->link(__('List Items'), ['action' => 'index'], ['class' => 'side-nav-item']); ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="items form content">
-            <?= $this->Form->create($item) ?>
+            <?= $this->Form->create($item); ?>
             <fieldset>
-                <legend><?= __('Add Item') ?></legend>
+                <legend><?= __('Add Item'); ?></legend>
                     <?= $this->Form->control('item_title'); ?>
                     <?= $this->Form->control('sale_price'); ?>
                     <?= $this->Form->control('is_in_stock'); ?>
                     <?= $this->Form->control('item_description'); ?>
                     <?= $this->Form->control('category_id'); ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+            <?= $this->Form->button(__('Submit')); ?>
+            <?= $this->Form->end(); ?>
         </div>
     </div>
 </div>
--- templates/Items/edit.php
+++ templates/Items/edit.php
@@ -8,18 +8,18 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
+            <h4 class="heading"><?= __('Actions'); ?></h4>
             <?= $this->Form->postLink(
                 __('Delete'),
                 ['action' => 'delete', $item->id],
                 ['confirm' => __('Are you sure you want to delete # {0}?', $item->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Items'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            ); ?>
+            <?= $this->Html->link(__('List Items'), ['action' => 'index'], ['class' => 'side-nav-item']); ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="items form content">
-            <?= $this->Form->create($item) ?>
+            <?= $this->Form->create($item); ?>
             <fieldset>
                 <legend><?= __('Edit Item') ?></legend>
                     <?= $this->Form->control('item_title'); ?>
@@ -31,8 +31,8 @@
                         'label' => 'Category',
                     ]); ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+            <?= $this->Form->button(__('Submit')); ?>
+            <?= $this->Form->end(); ?>
         </div>
     </div>
 </div>
